3bdaea82d30abd92aca00737578a37f8a1bf2874
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2022-01-30 19:18:36 +0100

    Index.

--- proyecto/frontend/flask-example/app/app.py
+++ proyecto/frontend/flask-example/app/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    return render_template('index.html', name="diego")
 
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
