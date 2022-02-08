bee66cdc3a69d669af0631ae24d1b01a8018b45c
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2022-02-08 11:30:38 +0100

    facedetect.

---
+++ proyecto/backend/facedetect/es.um.sisdist.facedetect/src/main/java/es/um/sisdist/facedetect/Example.java
@@ -0,0 +1,33 @@
+package es.um.sisdist.facedetect;
+
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.ColourSpace;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.image.typography.hershey.HersheyFont;
+
+/**
+ * OpenIMAJ Hello world!
+ *
+ */
+public class Example
+{
+    public static void main(String[] args)
+    {
+        // Create an image
+        MBFImage image = new MBFImage(320, 70, ColourSpace.RGB);
+
+        // Fill the image with white
+        image.fill(RGBColour.WHITE);
+
+        // Render some test into the image
+        image.drawText("Hello World", 10, 60, HersheyFont.CURSIVE, 50, RGBColour.BLACK);
+
+        // Apply a Gaussian blur
+        image.processInplace(new FGaussianConvolve(2f));
+
+        // Display the image
+        DisplayUtilities.display(image);
+    }
+}
---
+++ proyecto/backend/facedetect/es.um.sisdist.facedetect/src/test/java/es/um/ssdd/AppTest.java
@@ -0,0 +1,18 @@
+package es.um.ssdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
