6e3b6d0758aaaba73f5d1ef32640764f8993d76e
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2022-02-27 02:19:57 +0100

    Pequeños cambios.

--- impl/backend-rest/es.um.sisdist.videofaces.backend.Service/src/es/um/sisdist/videofaces/backend/Service/CheckLoginEndpoint.java
+++ impl/backend-rest/es.um.sisdist.videofaces.backend.Service/src/es/um/sisdist/videofaces/backend/Service/CheckLoginEndpoint.java
@@ -3,14 +3,10 @@ package es.um.sisdist.videofaces.backend.Service;
 import java.util.Optional;
 
 import es.um.sisdist.videofaces.backend.Service.impl.AppLogicImpl;
-import es.um.sisdist.videofaces.backend.dao.models.User;
 import es.um.sisdist.videofaces.models.UserDTO;
-import es.um.sisdist.videofaces.models.UserDTOUtils;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
