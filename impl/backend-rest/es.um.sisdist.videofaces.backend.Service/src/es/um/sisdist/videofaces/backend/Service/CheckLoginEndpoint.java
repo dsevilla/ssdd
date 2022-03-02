e03b9f3b9b1ec19c8f89a81652c701de8be17149
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2022-03-02 10:13:18 +0100

    Pequeño cambio en la implementación para que no gestione objetos DTO, sino sólo objetos del modelo DAO.

--- impl/backend-rest/es.um.sisdist.videofaces.backend.Service/src/es/um/sisdist/videofaces/backend/Service/CheckLoginEndpoint.java
+++ impl/backend-rest/es.um.sisdist.videofaces.backend.Service/src/es/um/sisdist/videofaces/backend/Service/CheckLoginEndpoint.java
@@ -3,9 +3,10 @@ package es.um.sisdist.videofaces.backend.Service;
 import java.util.Optional;
 
 import es.um.sisdist.videofaces.backend.Service.impl.AppLogicImpl;
+import es.um.sisdist.videofaces.backend.dao.models.User;
 import es.um.sisdist.videofaces.models.UserDTO;
+import es.um.sisdist.videofaces.models.UserDTOUtils;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -25,10 +26,10 @@ public class CheckLoginEndpoint
     @Produces(MediaType.APPLICATION_JSON)
     public Response checkUser(UserDTO uo)
     {
-    	Optional<UserDTO> u = impl.checkLogin(uo);
-    	if (u.isPresent())
-    		return Response.ok(u.get()).build();
-    	else
-    		return Response.status(Status.FORBIDDEN).build();
+        Optional<User> u = impl.checkLogin(uo.getEmail(), uo.getPassword());
+        if (u.isPresent())
+            return Response.ok(UserDTOUtils.toDTO(u.get())).build();
+        else
+            return Response.status(Status.FORBIDDEN).build();
     }
 }
--- impl/backend-rest/es.um.sisdist.videofaces.backend.Service/src/es/um/sisdist/videofaces/backend/Service/impl/AppLogicImpl.java
+++ impl/backend-rest/es.um.sisdist.videofaces.backend.Service/src/es/um/sisdist/videofaces/backend/Service/impl/AppLogicImpl.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package es.um.sisdist.videofaces.backend.Service.impl;
 
@@ -13,8 +13,6 @@ import es.um.sisdist.videofaces.backend.dao.user.IUserDAO;
 import es.um.sisdist.videofaces.backend.grpc.GrpcServiceGrpc;
 import es.um.sisdist.videofaces.backend.grpc.VideoAvailability;
 import es.um.sisdist.videofaces.backend.grpc.VideoSpec;
-import es.um.sisdist.videofaces.models.UserDTO;
-import es.um.sisdist.videofaces.models.UserDTOUtils;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -26,74 +24,71 @@ public class AppLogicImpl
 {
     IDAOFactory daoFactory;
     IUserDAO dao;
-    
+
     private static final Logger logger = Logger.getLogger(AppLogicImpl.class.getName());
 
     private final ManagedChannel channel;
     private final GrpcServiceGrpc.GrpcServiceBlockingStub blockingStub;
     private final GrpcServiceGrpc.GrpcServiceStub asyncStub;
-    
-	static AppLogicImpl instance = new AppLogicImpl();
-	
-	private AppLogicImpl()
-	{
-		daoFactory = new DAOFactoryImpl();
-		dao = daoFactory.createSQLUserDAO();
-		
-		Optional<String> grpcServerName = 
-				Optional.ofNullable(System.getenv("GRPC_SERVER"));
-		Optional<String> grpcServerPort = 
-				Optional.ofNullable(System.getenv("GRPC_SERVER_PORT"));
-		
-	    channel = ManagedChannelBuilder.forAddress(grpcServerName.orElse("localhost"),
-	    		Integer.parseInt(grpcServerPort.orElse("50051")))
-	            // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
-	            // needing certificates.
-	            .usePlaintext()
-	            .build();
-	        blockingStub = GrpcServiceGrpc.newBlockingStub(channel);
-	        asyncStub = GrpcServiceGrpc.newStub(channel);
-	}
-	
-	public static AppLogicImpl getInstance()
-	{
-		return instance;
-	}
-	
-	public Optional<User> getUserByEmail(String userId)
-	{
-		Optional<User> u = dao.getUserByEmail(userId);
-		return u;
-	}
 
-	public Optional<User> getUserById(String userId)
-	{
-		return dao.getUserById(userId);
-	}
+    static AppLogicImpl instance = new AppLogicImpl();
 
-	public boolean isVideoReady(String videoId)
-	{
-		// Test de grpc, puede hacerse con la BD
-		VideoAvailability available = 
-				blockingStub.isVideoReady(VideoSpec.newBuilder().setId(videoId).build());
-		return available.getAvailable();
-	}
-	
-	// El frontend, a través del formulario de login, 
-	// envía el usuario y pass, que se convierte a un DTO. De ahí
-	// obtenemos la consulta a la base de datos, que nos retornará,
-	// si procede, 
-	public Optional<UserDTO> checkLogin(UserDTO userToCheck)
-	{
-		Optional<User> u = dao.getUserByEmail(userToCheck.getEmail());
-		
-		if (u.isPresent())
-		{
-			String hashed_pass = User.md5pass(userToCheck.getPassword());
-			if (0 == hashed_pass.compareTo(u.get().getPassword_hash()))
-				return Optional.of(UserDTOUtils.toDTO(u.get()));
-		}
-		
-		return Optional.empty();
-	}
+    private AppLogicImpl()
+    {
+        daoFactory = new DAOFactoryImpl();
+        dao = daoFactory.createSQLUserDAO();
+
+        Optional<String> grpcServerName = Optional.ofNullable(System.getenv("GRPC_SERVER"));
+        Optional<String> grpcServerPort = Optional.ofNullable(System.getenv("GRPC_SERVER_PORT"));
+
+        channel = ManagedChannelBuilder
+                .forAddress(grpcServerName.orElse("localhost"), Integer.parseInt(grpcServerPort.orElse("50051")))
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS
+                // to avoid
+                // needing certificates.
+                .usePlaintext().build();
+        blockingStub = GrpcServiceGrpc.newBlockingStub(channel);
+        asyncStub = GrpcServiceGrpc.newStub(channel);
+    }
+
+    public static AppLogicImpl getInstance()
+    {
+        return instance;
+    }
+
+    public Optional<User> getUserByEmail(String userId)
+    {
+        Optional<User> u = dao.getUserByEmail(userId);
+        return u;
+    }
+
+    public Optional<User> getUserById(String userId)
+    {
+        return dao.getUserById(userId);
+    }
+
+    public boolean isVideoReady(String videoId)
+    {
+        // Test de grpc, puede hacerse con la BD
+        VideoAvailability available = blockingStub.isVideoReady(VideoSpec.newBuilder().setId(videoId).build());
+        return available.getAvailable();
+    }
+
+    // El frontend, a través del formulario de login,
+    // envía el usuario y pass, que se convierte a un DTO. De ahí
+    // obtenemos la consulta a la base de datos, que nos retornará,
+    // si procede,
+    public Optional<User> checkLogin(String email, String pass)
+    {
+        Optional<User> u = dao.getUserByEmail(email);
+
+        if (u.isPresent())
+        {
+            String hashed_pass = User.md5pass(pass);
+            if (0 == hashed_pass.compareTo(u.get().getPassword_hash()))
+                return u;
+        }
+
+        return Optional.empty();
+    }
 }
