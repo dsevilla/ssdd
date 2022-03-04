ff4c2cbdd747f180ebf761e1e12a41e3f9acb9b4
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2022-03-04 19:24:41 +0100

    Maven ahora no ejecuta los tests al compilar, sino cuando se le pasa mvn test.

--- impl/backend-rest/es.um.sisdist.videofaces.backend.Service/tests/java/tests/es/um/sisdist/videofaces/backend/Service/impl/tests/AppLogicImplTest.java
+++ impl/backend-rest/es.um.sisdist.videofaces.backend.Service/tests/java/tests/es/um/sisdist/videofaces/backend/Service/impl/tests/AppLogicImplTest.java
@@ -9,20 +9,18 @@ package es.um.sisdist.videofaces.backend.Service.impl.tests;
  */
 class AppLogicImplTest
 {
-    // Deshabilitados por ahora ya que necesitan de conexión a BD.
-//	static AppLogicImpl impl;
-//
-//	@BeforeAll
-//	static void setup()
-//	{
-//		impl = AppLogicImpl.getInstance();
-//	}
-//
-//	@Test
-//	void testDefaultUser()
-//	{
-//		Optional<User> u = impl.getUserByEmail("dsevilla@um.es");
-//		assertEquals(u.get().getEmail(), "dsevilla@um.es");
-//	}
-//
+        static AppLogicImpl impl;
+
+        @BeforeAll
+        static void setup()
+        {
+                impl = AppLogicImpl.getInstance();
+        }
+
+        @Test
+        void testDefaultUser()
+        {
+                Optional<User> u = impl.getUserByEmail("dsevilla@um.es");
+                assertEquals(u.get().getEmail(), "dsevilla@um.es");
+        }
 }
