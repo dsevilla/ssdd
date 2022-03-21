e91899cebb16e54eb7dbcef5a12654083b06a45d
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2022-03-21 20:00:50 +0100

    Varias cosas:
    - Contenedor para Mongo
    - Añadidos ficheros de environment a los docker-compose para que sea más fácil cambiar de mongo a sql

---
+++ impl/videofaces-mongo/initialize.js
@@ -0,0 +1,9 @@
+use videofaces;
+
+db.users.insertOne({id: "ddd", 
+	email: "dsevilla@um.es",
+	password_hash: "21232f297a57a5a743894a0e4a801fc3",
+	name: "diego",
+	token: "TOKEN",
+	visits: 0});
+"
