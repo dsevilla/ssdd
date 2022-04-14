3ae59a171c00e94fb9f10008eb9b03e7767c7c75
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2022-04-14 02:45:45 +0200

    Pruebas con multipart.

--- impl/backend-rest/es.um.sisdist.videofaces.backend.Service/src/es/um/sisdist/videofaces/backend/Service/ServiceApp.java
+++ impl/backend-rest/es.um.sisdist.videofaces.backend.Service/src/es/um/sisdist/videofaces/backend/Service/ServiceApp.java
@@ -1,19 +1,22 @@
 package es.um.sisdist.videofaces.backend.Service;
 
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+// https://stackoverflow.com/a/45627178/62365
+
 @ApplicationPath("/")
 public class ServiceApp extends Application
 {
+    @Override
+    public Map<String, Object> getProperties()
+    {
+        Map<String, Object> props = new HashMap<>();
+        props.put("jersey.config.server.provider.classnames", "org.glassfish.jersey.media.multipart.MultiPartFeature");
+        return props;
+    }
 
-	@Override
-	public Set<Object> getSingletons()
-	{
-		// TODO Auto-generated method stub
-		return super.getSingletons();
-	}
-    // Vacía a propósito.
 }
