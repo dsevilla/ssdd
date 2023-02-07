e3672eb70f7ea6d38a7a54d87c4e97f0f21121dc
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2023-02-07 13:17:01 +0100

    cambios a validadores

--- proyecto/frontend/app/forms.py
+++ proyecto/frontend/app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, BooleanField, FileField)
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import InputRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    email = StringField('email', validators=[Email()])
+    password = PasswordField('password', validators=[InputRequired()])
     remember_me = BooleanField('Recuérdame')
 
