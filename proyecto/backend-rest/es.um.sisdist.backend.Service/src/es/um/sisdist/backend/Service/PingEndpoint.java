6cae5d50f223af0c501e642d654cf069a2bd4b64
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2023-02-15 01:43:22 +0100

    Versiones de librerías.

--- proyecto/backend-rest/es.um.sisdist.backend.Service/src/es/um/sisdist/backend/Service/PingEndpoint.java
+++ proyecto/backend-rest/es.um.sisdist.backend.Service/src/es/um/sisdist/backend/Service/PingEndpoint.java
@@ -1,13 +1,11 @@
 package es.um.sisdist.backend.Service;
 
 import es.um.sisdist.backend.Service.impl.AppLogicImpl;
-import es.um.sisdist.models.UserDTO;
-import es.um.sisdist.models.UserDTOUtils;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/ping")
 public class PingEndpoint
@@ -15,9 +13,9 @@ public class PingEndpoint
     private AppLogicImpl impl = AppLogicImpl.getInstance();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public boolean ping()
+    @Produces({MediaType.APPLICATION_JSON,MediaType.TEXT_PLAIN})
+    public Response ping()
     {
-        return impl.ping(1);
+        return Response.ok(impl.ping(1)).build();
     }
 }
