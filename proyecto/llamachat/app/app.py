c29073f04169100278a7807c5eecd445e67029c8
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2024-12-12 10:17:37 +0100

    Some cleanups:
    - python 3.12 alpine images that are smaller
    - code cleanup
    - requirements update

--- proyecto/llamachat/app/app.py
+++ proyecto/llamachat/app/app.py
@@ -1,3 +1,4 @@
+from typing import cast, override
 import uuid
 import http
 import time
@@ -14,76 +15,81 @@ class LLM_base:
     # Prompt request stack
     prompt_stack = []
     prompt_map_lock = Lock()  # Will be used for both the map and the stack
-    
-    def init_model(self):
-        pass
 
-    def llm(self, token, prompt):
-        pass
+    def init_model(self) -> None:
+        ...
 
-if os.environ.get('DUMMY'):
-    class LLM(LLM_base):
+    def llm(self, token, prompt) -> str:
+        ...
 
-        def init_model(self):
+class LLM(LLM_base):
+
+    if os.environ.get('DUMMY'):
+
+        @override
+        def init_model(self) -> None:
             time.sleep(5)
             self.llm_set = True
 
-        def llm(self, token, prompt):
+        @override
+        def llm(self, token: str, prompt: str) -> str:
             time.sleep(5)
             return f"""DUMMY Generated response for token "{token}"."""
-else:
-    class LLM(LLM_base):
 
-        def init_model(self):
+    else:
+
+        @override
+        def init_model(self) -> None:
+            from ctransformers import llm
             from ctransformers import AutoModelForCausalLM
 
             # Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
-            self._llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7b-Chat-GGUF",
+            self._llm: llm.LLM = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7b-Chat-GGUF",
                                                         model_file="llama-2-7b-chat.Q4_K_M.gguf",
                                                         model_type="llama",
                                                         gpu_layers=0)
             self.llm_set = True
 
-        def llm(self, token, prompt):
-            return self._llm(prompt)
-        
-        
-llm = LLM()
+        @override
+        def llm(self, token: str, prompt: str) -> str:
+            return cast(str, self._llm(prompt, stream=False)) # We cast to str because we set stream=False
+
+
+the_llm = LLM()
 
 
 def init_model_and_process_requests():
-    global llm
+    global the_llm
 
-    llm.init_model()
+    the_llm.init_model()
 
     # infinite loop
     while True:
-        llm.prompt_map_lock.acquire()
-        if len(llm.prompt_stack) == 0:
-            llm.prompt_map_lock.release()
+        the_llm.prompt_map_lock.acquire()
+        if len(the_llm.prompt_stack) == 0:
+            the_llm.prompt_map_lock.release()
             time.sleep(.1)
         else:
-            (token, prompt) = llm.prompt_stack.pop(0)
-            llm.prompt_map_lock.release()
+            (token, prompt) = the_llm.prompt_stack.pop(0)
+            the_llm.prompt_map_lock.release()
 
             # Generate a response
-            prompt['answer'] = llm.llm(token, prompt['prompt'].strip())
+            prompt['answer'] = the_llm.llm(token, prompt['prompt'].strip())
 
 Thread(target=init_model_and_process_requests).start()
 
-def handle_response_request(prompt: dict) -> dict:
+def handle_response_request(prompt: dict) -> str:
     # Generate UUID for request
     token = str(uuid.uuid4())
     # Lock the access to the map
-    llm.prompt_map_lock.acquire()
+    the_llm.prompt_map_lock.acquire()
     # Push the pending work on the stack
-    llm.prompt_stack.append((token, prompt))
+    the_llm.prompt_stack.append((token, prompt))
     # And add it incomplete to the prompt map
-    llm.prompt_map[token] = prompt
-    llm.prompt_map_lock.release()
-    
-    return token
+    the_llm.prompt_map[token] = prompt
+    the_llm.prompt_map_lock.release()
 
+    return token
 
 app = Flask(__name__, static_url_path='')
 
@@ -100,13 +106,13 @@ def index():
 @app.route('/prompt', methods=['GET', 'POST'])
 def prompt():
     if request.method == "POST":
-        if not llm.llm_set:
+        if not the_llm.llm_set:
             return Response('Still initializing...\n', http.HTTPStatus.PROCESSING)
         if not request.is_json or not request.json['prompt']:
             return Response('application/json type required, with {"prompt": "..."} format.\n',
                              http.HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
-        token = handle_response_request(request.json)
-        return Response('Accepted\n', 
+        token: str = handle_response_request(request.json)
+        return Response('Accepted\n',
                         status=http.HTTPStatus.ACCEPTED,
                         headers={"Location": f"/response/{token}"})
     else:
@@ -114,11 +120,11 @@ def prompt():
 
 @app.route('/response/<token>', methods=['GET'])
 def resp(token):
-    if not llm.llm_set:
+    if not the_llm.llm_set:
         return Response('Still initializing...\n', http.HTTPStatus.NO_CONTENT)
-    llm.prompt_map_lock.acquire()
-    res = llm.prompt_map.get(str(token), None)
-    llm.prompt_map_lock.release()
+    the_llm.prompt_map_lock.acquire()
+    res = the_llm.prompt_map.get(str(token), None)
+    the_llm.prompt_map_lock.release()
     if not res:
         return Response('Token unknown.\n', http.HTTPStatus.NOT_FOUND)
     if not res.get('answer', None):
@@ -129,7 +135,7 @@ def resp(token):
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
-    if not llm.llm_set:
+    if not the_llm.llm_set:
         return Response(status=http.HTTPStatus.NO_CONTENT)
     return Response(status=http.HTTPStatus.OK)
 
--- proyecto/llamachat/initmodel.py
+++ proyecto/llamachat/initmodel.py
@@ -1,7 +1,7 @@
 from ctransformers import AutoModelForCausalLM
+from ctransformers.llm import LLM
 
-llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7b-Chat-GGUF",
+llm: LLM = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7b-Chat-GGUF",
                                            model_file="llama-2-7b-chat.Q4_K_M.gguf",
                                            model_type="llama",
                                            gpu_layers=0)
-
